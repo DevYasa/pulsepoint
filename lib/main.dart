dd79e0a7b7aedae5c5d8d2c3d17357b3ecbe3b2e
DevYasa <mhdyasir132@gmail.com>
2024-06-04 12:34:28 +0530

    updatee

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:pulsepoint/signup/signup_page.dart';
 import 'package:pulsepoint/home/home.dart';
 //import 'package:pulsepoint/home/donate_blood_page.dart';
 import 'package:pulsepoint/home_screen/home_screen.dart';
+import 'package:pulsepoint/welcome/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/': (context) =>
-            HomePage(), // This sets the HomeScreen as the initial route
+            const WelcomePage(), // This sets the HomeScreen as the initial route
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignupPage(),
         '/home_screen': (context) => const HomeScreen(),
