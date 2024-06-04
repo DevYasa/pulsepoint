e4fd5e3221b486ff6e0ac72c8cfd679f200b7fbf
DevYasa <mhdyasir132@gmail.com>
2024-06-04 13:24:28 +0530

    update donor login

--- lib/home_screen/donor_login_screen.dart
+++ lib/home_screen/donor_login_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class DonorLoginScreen extends StatelessWidget {
+  const DonorLoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Implement the UI for the Donor login screen here
-    return Container(
-      child: Text('This is the Donor login screen'),
-    );
+    return const Text('This is the Donor login screen');
   }
 }
